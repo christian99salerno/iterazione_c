457a063280970a20dc0fdbc91af147f8ded49cfd
christian99salerno <christian.salerno05@gmail.com>
2017-05-27 16:00:35 +0200

    Programma Countdown con time.h in aggiunta

---
+++ countdown.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+
+struct timespec intervallo;
+intervallo.tv_sec = 1;
+intervallo.tv_nsec = 0 * 1000 * 1000;
+
+int num;
+
+printf("Inserisci un numero da cui partire: ");
+scanf("%d", &num);
+
+while(num!=-1)
+{
+  printf("Countdown: %d\n", num);
+  nanosleep(&intervallo, NULL);
+  num--;
+
+}
+ return 0;
+ }
