87595c9a41b2dbbf274c26ebc14f478f56a6c32d
alunno <alunno@lab03-03>
2017-05-30 10:45:46 +0200

    tabelline

---
+++ tabelline.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+ 
+int i=1;
+
+while (i<=10)
+{
+  int j=1;
+  while (j<=10){
+    int tabellina=i*j;
+    printf("%d x %d= ", i, j); 
+    printf("%d\t", tabellina);
+    j++;
+  }
+i++;
+    printf("\n");
+} 
+  return 0;
+}
