00e757f787484369afab485b3e3c67c661cb7718
christian99salerno <christian.salerno05@gmail.com>
2017-05-30 16:41:19 +0200

    tabellina 0.0

--- tabelline.c
+++ tabelline.c
@@ -3,16 +3,17 @@
 int main () {
  
 int i=1;
+int j=1;
+int tabellina;
 
 while (i<=10)
 {
-  int j=1;
   while (j<=10){
-    int tabellina=i*j;
-    printf("%d x %d = ", j, i); 
+    tabellina=i*j;
+    printf("%d x %d = ", i, j); 
     printf("%d\t", tabellina);
     j++;
-  }
+}
 i++;
     printf("\n");
 } 
