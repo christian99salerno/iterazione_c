ed4f662582efd6d99a71422dc14b8f5ba99ff63b
Billie99 <billiearmstrong1886@gmail.com>
2017-06-06 09:39:24 +0200

    da controllare quadratovuoto

---
+++ quadratovuoto.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main ()
+{
+  int num = 1;
+
+  int i, j;
+
+  do{
+
+    printf("Inserisci il lato (min 1, max 20): ");
+    scanf("%d", &num);
+
+    if(num < 1 || num > 20)
+      printf("Valore fuori dai limiti!\n");
+
+  }
+
+  while(num < 1 || num > 20);
+
+  i = 0;
+  while(i < num){
+      printf("*");
+        i++;
+  }
+      printf("\n");
+i = 0;
+   while(i < num){
+    printf("*");
+     j=1;
+   while(j < num-1){
+    printf(" ");
+        j++;
+    }
+   printf("*");
+   printf("\n");
+   i++;
+   }
+i=0; 
+  while(i < num){
+      printf("*");
+        i++;
+  }
+printf("\n");       
+              return 0;
+}
+
