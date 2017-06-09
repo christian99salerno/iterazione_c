c86643856ac8768f65a56ea6c119e42dc99c8afd
Billie99 <billiearmstrong1886@gmail.com>
2017-06-09 08:29:28 +0200

    Palindromo

--- palindromo.c
+++ palindromo.c
@@ -7,24 +7,29 @@ int num = 0;
 
 int c = 0;
 int d = 0;
+int e = 0;
+int f = 0;
 
 do{
   
   printf("Inserisci un numero ");
   scanf("%d", &num);
 
-  if(num<10000)
+  if(num<10000 && num >= 100000)
     printf("Inserisci un num di 5 cifre\n");
 }
 
-while (num < 10000);
+while (num < 10000 && num >= 100000);
 
 c = num%10;
 num/=10;
 d = num%10;
+num/=100;
+e = num%10;
 num/=10;
-  
-if(num/10 == c)
+f = num;
+
+if(e == d && f == c)
    printf("Il numero e' palindromo\n");
 
 else
