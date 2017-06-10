5edcdd8b7c6b8d3f52a50b331ddb368cfda27c81
christian99salerno <christian.salerno05@gmail.com>
2017-06-10 21:28:44 +0200

    Problemi con i controlli

--- binario.c
+++ binario.c
@@ -5,13 +5,28 @@ int main () {
 
 int num = 0;
 
+int bit = 0;
+
 do {
 
   printf("Inserisci un numero binario ");
   scanf("%d", &num);
 
+  bit = 1;
 
+  while (num!=0){
+  if (num%10 != 1 || num%10 != 0){
+    bit = 0;
+  }
+    num/=10;
+   }
+  if (bit==0)
+    printf("Il numero deve essere binario\n");
+}
+while(bit==0);
 
+if (bit == 1)
+  printf("Il numero e' binario\n");
   
   return 0;
 }
