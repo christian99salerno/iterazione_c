cbf820f860a0f57ed99366dc4297981b05d8e4d9
christian99salerno <christian.salerno05@gmail.com>
2017-08-01 20:09:12 +0200

    cripta

---
+++ cripta.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main () 
+{
+
+int num = 0;
+
+int a,b,c,d;
+
+do{
+
+printf("Numero da crittografare: ");
+scanf("%d",&num);
+
+if (num < 1000 || num > 9999)
+printf("Reinserisci il numero con 4 cifre\n");
+}
+while (num < 1000 || num > 9999);
+
+ d = (num%10 + 7)%10;
+ num/=10;
+ c = (num%10 + 7)%10;
+ num/=10;
+ b = (num%10 + 7)%10;
+ num/=10;
+ a = (num%10 + 7)%10;
+ num/=10;
+ 
+ num = c;
+ num = ((num*10+d)*10+a)*10+b;
+
+ printf("Numero crittografato: %d\n",num);
+
+  return 0;
+}
