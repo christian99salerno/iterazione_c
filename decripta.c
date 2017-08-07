41f4d7dfe5636412e69f18621860f737db68b05f
christian99salerno <christian.salerno05@gmail.com>
2017-08-07 20:28:51 +0200

    Decripta

--- decripta.c
+++ decripta.c
@@ -9,25 +9,33 @@ int a,b,c,d;
 
 do{
 
-printf("Numero da decrittografare: ");
+printf("Numero criptato: ");
 scanf("%d",&num);
 
-if (num < 1000 || num > 9999)
+if (num < 0 || num > 9999)
 printf("Reinserisci il numero con 4 cifre\n");
 }
-while (num < 1000 || num > 9999);
+while (num < 0 || num > 9999);
 
- d = (num%10 - 7);
+ d = num%10; 
+ d = ((d+10)-7)%10; 
+ num/=10; 
+
+ c = num%10;
+ c = ((c+10)-7)%10;
+ num/=10; 
+
+ b = num%10;
+ b = ((b+10)-7)%10;
+ num/=10; 
+
+ a = num%10;
+ a = ((a+10)-7)%10;
  num/=10;
- c = (num%10 - 7);
- num/=10;
- b = (num%10 - 7);
- num/=10;
- a = (num%10 - 7);
- num/=10;
+
+ /*num = 0*/
  
- num = c;
- num = ((num*10+d)*10+a)*10+b;
+ num = ((c*10+d)*10+a)*10+b;
 
  printf("Numero originale: %d\n",num);
 
