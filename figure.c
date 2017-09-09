6455a051a2b993058c6f051bfcfde8419ab025e7
Christian Salerno <christian.salerno05@gmail.com>
2017-09-09 06:50:18 +0200

    figure completate

--- figure.c
+++ figure.c
@@ -9,6 +9,10 @@ int i;
 int j;
 int max_a = 1;
 int max_b = 10;
+int max_c = 10;
+int max_d = 1;
+
+printf("\n");
 
 printf("a)\n");
 for (i=0;i<10;i++){
@@ -19,6 +23,7 @@ for (i=0;i<10;i++){
       max_a++;
   }
 
+printf("\n");
 
 printf("b)\n");
 for (i=0;i<10;i++){
@@ -29,7 +34,29 @@ for (i=0;i<10;i++){
       max_b--;
   }
 
+printf("\n");
 
+printf("c)\n");
+for (i=0;i<10;i++){
+  for(j=max_c;j>=1;j--){
+      putchar(carattere);
+    }
+      printf("\n");
+      max_c--;
+  }
+
+printf("\n");
+
+printf("d)\n");
+for (i=0;i<10;i++){
+  for(j=1;j<=max_d;j++){
+      putchar(carattere);
+    }
+      printf("\n");
+      max_d++;
+  }
+
+printf("\n");
 
  return 0;
 }
