f7a6e0329403755999a97793515ae54ec53280c4
Christian <christian.salerno05@gmail.com>
2018-10-31 08:49:13 +0100

    risolvendo i problemi alla mia maniera

--- figure.c
+++ figure.c
@@ -7,10 +7,14 @@ int carattere = '*';
 
 int i;
 int j;
+int w;
+int x;
 int max_a = 1;
 int max_b = 10;
 int max_c = 10;
 int max_d = 1;
+int max_w = 1;
+int max_x = 10;
 
 printf("\n");
 
@@ -39,21 +43,32 @@ printf("\n");
 printf("c)\n");
 for (i=0;i<10;i++){
   for(j=max_c;j>=1;j--){
-      putchar(carattere);
+    putchar(carattere);
     }
-      printf("\n");
-      max_c--;
-  }
+  printf("\n");
+  for(w=1;w<=max_w;w++)
+  {
+    printf(" ");
+   }
+    max_w++;
+  max_c--;
+}
 
 printf("\n");
 
 printf("d)\n");
 for (i=0;i<10;i++){
+  
+  for(x=max_x;x>1;x--)
+  {
+    printf(" ");
+   }
   for(j=1;j<=max_d;j++){
       putchar(carattere);
     }
       printf("\n");
-      max_d++;
+    max_x--;
+    max_d++;
   }
 
 printf("\n");
