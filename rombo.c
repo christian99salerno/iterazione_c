f98a69bf68e1491840b8fc332557013d2aa4ac66
Christian <christian.salerno05@gmail.com>
2018-12-16 10:53:29 +0100

    muori rombo

--- rombo.c
+++ rombo.c
@@ -3,22 +3,49 @@
 int main ()
 {
 
+int i;
+int j;
 int num = 0;
-int carattere = "*";
-
+int carattere = '*';
+int c = 1;
+int m = 0;
+int space = 0;
+int z;
 
 do{
 	printf("Numero di righe (dispari 1-19): ");
 	scanf("%d",&num);
 
-        if(num%2==0 || num<2 || num>18){
-		printf("Inserisci un num dispari 1<num<19: ");
-	}
 	 }
-	while(num%2==0 || num<2 || num>18);
+	while(num%2==0 || num<1 || num>19);
 
+space = num/2;
 
+for(i=1; i<=num; i++){
+  for(z=1; z<=space; z++){
+    printf(" ");
+  }
+  for(j=1; j<=c; j++){
+    putchar(carattere);
+    /*printf("%d",j);*/
+  }
+  printf("\n");
 
+if(c>=1 && c<num && m!=1){
+   c+=2;
+   space--;
+ }
+
+else{
+    c-=2;
+    space++;
+ }
+
+if(c == num){
+   m=1;
+  }
+
+}
 
 	return 0;
 }
