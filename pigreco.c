073cf6713070646174886f98286e66767f1bc02a
Christian <christian.salerno05@gmail.com>
2018-12-16 12:29:33 +0100

    un signore di nome pi greco

---
+++ pigreco.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main () {
+
+int num = 0;
+double pg = 0;
+int i;
+int m = 1;
+double n = 1.0;
+
+printf("\n");
+printf("Approssimazione di Pi Greco mediante la serie:\n");
+printf("\n");
+printf("4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + ...\n");
+printf("\n");
+printf("Quanti termini vuoi utilizzare? ");
+scanf("%d", &num);
+
+for(i=0; i<num; i++)
+{
+
+  if(i%2==0){
+    m = 1;
+  }
+
+  else{
+    m = -1;
+  }
+
+  pg = pg + (4.0/n)*m;
+
+  n+=2;
+
+}
+
+printf("\n");
+printf("PI: %f\n", pg);
+printf("\n");
+
+  return 0;
+}
