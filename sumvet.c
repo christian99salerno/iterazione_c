2ce5d9539290dc85387f50789c924f03c04f3cc5
Christian <christian.salerno05@gmail.com>
2018-12-23 18:37:56 +0100

    sumvet

---
+++ sumvet.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main () {
+
+int i;
+int numbers[10];
+int somma = 0;
+double media = 0;
+
+printf("\n");
+
+printf("Somma e Media\n");
+
+printf("\n");
+
+for(i=1; i<=10; i++) {
+
+printf("? ");
+scanf("%d", &numbers[i]);
+
+somma+=numbers[i];
+
+}
+
+media=somma/10;
+
+printf("\n");
+
+for(i=1; i<=10; i++) {
+
+  printf("number[%d] = %d\n", i, numbers[i]);
+
+}
+
+printf("\n");
+
+printf("Somma: %d\n", somma);
+printf("Media: %f\n", media);
+
+  return 0;
+
+}
