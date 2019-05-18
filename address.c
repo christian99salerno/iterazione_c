0cdf9b37c3b47259b0237939886f48d5df4475b4
Christian <christian.salerno05@gmail.com>
2019-05-18 15:46:57 +0200

    tolto warning

--- address.c
+++ address.c
@@ -4,11 +4,11 @@ int main ()
 
 {
 
-  int vet[10];
   int i;
+  int vet[10];
 
   for(i=0; i<=9; i++){
-    printf("indirizzo di num[%d]: %p\n", i, &vet[i]);
+    printf("indirizzo di num[%d]: %p\n", i, (void*)&vet[i] );
 }
 
 return 0;
